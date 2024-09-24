bc853b3717e714f6558cb7139879e4a9c669eb58
diegobreeg <diegobreeg@gmail.com>
2024-09-24 12:00:15 -0300

    project adjustments

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (_, res: Response) =>
 {
     res.send('hello world!')
 })
 
-app.listen(PORT, () =>
+app.listen(process.env.PORT, () =>
 {
-    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
 })
